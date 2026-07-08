2eaf2bc0b2394ef7199b5cc4e9a15530389f89f3
Wesley <you@example.com>
2026-07-07 22:43:09 -0300

    Aponta cliente para tunel playit

--- Network/GameNetwork.cs
+++ Network/GameNetwork.cs
@@ -9,8 +9,8 @@ using System.Text.Json;
 
 public partial class GameNetwork : Node
 {
-    private const string DefaultServerHost = "191.54.77.168";
-    private const int DefaultServerPort = 7777;
+    private const string DefaultServerHost = "maintenance-aid.gl.at.ply.gg";
+    private const int DefaultServerPort = 49027;
 
     public static bool AutoLogin = false;
 
